acd7fdffd3e715df928fa6c07296d18dfec55259
mohamednagy54 <92611426+mohamednagy54@users.noreply.github.com>
2022-09-28 11:16:30 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+
+
+// select the elements
+
+let toggle_btn;
+let big_wrapper;
+let hamburger_menu;
+
+function declare(){
+toggle_btn = document.querySelector('.toggle-btn')
+big_wrapper = document.querySelector('.big-wrapper')
+hamburger_menu = document.querySelector('.hamburger-menu')
+
+
+}
+declare()
+
+const main =document.querySelector('main');
+
+
+let dark = false;
+
+function toggleAnimation(){
+    // clone the landing page (wrapper)
+
+    dark = !dark
+
+    let  clone = big_wrapper.cloneNode(true);
+    if (dark === true) {
+        clone.classList.remove("light")
+        clone.classList.add("dark")
+        
+    }else{
+        clone.classList.remove("dark")
+        clone.classList.add("light")
+    }
+
+    clone.classList.add('copy');
+    main.appendChild(clone);
+
+    document.body.classList.add('stop-scrolling');
+
+    clone.addEventListener("animationend", () => {
+    document.body.classList.remove('stop-scrolling');
+
+        big_wrapper.remove();
+        clone.classList.remove('copy')
+
+        // Reset Variables
+        declare()
+        events()
+
+        
+        
+    })
+    
+
+}
+
+
+function events(){
+    toggle_btn.addEventListener('click',toggleAnimation)
+    hamburger_menu.addEventListener("click",() => {
+        big_wrapper.classList.toggle("active");
+
+        
+    })
+
+}
+
+events()
+
